778a296aecdea51838dde9dd0ddc9af44ce99357
steeven-js <jacques.steeven@gmail.com>
2026-01-23 08:57:14 -0400

    Initial Commit

---
+++ madiniamobileapp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  madiniamobileapp
+//
+//  Created by Steeven on 23/01/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ madiniamobileappTests/madiniamobileappTests.swift
@@ -0,0 +1,16 @@
+//
+//  madiniamobileappTests.swift
+//  madiniamobileappTests
+//
+//  Created by Steeven on 23/01/2026.
+//
+
+import Testing
+
+struct madiniamobileappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
